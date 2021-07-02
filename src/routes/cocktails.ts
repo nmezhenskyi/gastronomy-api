c97dbe1997f1851f710deae8d5bd5c916c113796
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-01 20:56:00 -0400

    /cocktails GET and POST endpoints added

--- src/routes/cocktails.ts
+++ src/routes/cocktails.ts
@@ -1,26 +1,71 @@
 import express from 'express'
-//import { body, validationResult } from 'express-validator'
+import { body, validationResult } from 'express-validator'
 import CocktailService from '../services/CocktailService'
 
 const router = express.Router()
 
 /**
+ * Retrieve cocktails.
+ * 
  * @route   GET /cocktails
+ * @access  Public
  */
 router.get('/', async (_, res) => {
    const result = await CocktailService.find()
 
-   if (!result.success) return res.status(500).json({ message: result.message })
+   if (result.message === 'FAILED') return res.status(500).json({ message: result.message })
 
-   return res.status(200).json({ body: result.body })
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: result.message })
+
+   return res.status(200).json(result.body)
 })
 
 /**
+ * Retrieve the cocktail by id.
+ * 
+ * @route   GET /cocktails/:id
+ * @access  Public
+ */
+router.get('/:id', async (req, res) => {
+   const result = await CocktailService.findOne({ id: req.params.id })
+
+   if (result.message === 'FAILED') return res.status(500).json({ message: result.message })
+
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: result.message })
+
+   return res.status(200).json(result.body)
+})
+
+/**
+ * Create new cocktail.
+ * 
  * @route   POST /cocktails
+ * @access  Public
  */
 router.post('/',
-async (_, res) => {
-   return res.status(200).json({ message: 'POST /cocktails' })
+body('name').notEmpty().isLength({ max: 100 }).trim(),
+body('description').isLength({ max: 5000 }).trim(),
+body('method').notEmpty().isLength({ max: 3000 }).trim(),
+body('notesOnIngredients').isLength({ max: 2000 }).trim(),
+body('notesOnExecution').isLength({ max: 2000 }).trim(),
+body('notesOnTaste').isLength({ max: 1000 }).trim(),
+async (req, res) => {
+   const errors = validationResult(req)
+   if (!errors.isEmpty())
+      return res.status(400).json({ message: 'Invalid data', errors: errors.array() })
+
+   const result = await CocktailService.create({
+      name: req.body.name,
+      description: req.body.description,
+      method: req.body.method,
+      notesOnIngredients: req.body.notesOnIngredients,
+      notesOnExecution: req.body.notesOnExecution,
+      notesOnTaste: req.body.notesOnTaste
+   })
+
+   if (result.message === 'FAILED') return res.status(500).json({ message: `Server Error: Couldn't create cocktail` })
+
+   return res.status(200).json(result.body)
 })
 
 export default router

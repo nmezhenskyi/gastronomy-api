79a7ff6a50f03cdeaf30f001c5dfb313f9204208
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-17 12:09:33 -0400

    Removed ormconfig.json, created main config file (/confing/default.toml)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata'
 import express from 'express'
-import { createConnection } from 'typeorm'
-import { PORT } from './common/constants'
+import config from 'config'
+import { createConnection, ConnectionOptions } from 'typeorm'
+import { PORT, PROD } from './common/constants'
 import rootRoute from './routes/root'
 import ingredientsRoute from './routes/ingredients'
 import cocktailsRoute from './routes/cocktails'
@@ -10,7 +11,18 @@ const app = express()
 
 const start = async () => {
    try {
-      await createConnection()
+      await createConnection({
+         type: config.get('database.type'),
+         host: config.get('database.host'),
+         port: config.get('database.port'),
+         username: config.get('database.username'),
+         password: config.get('database.password'),
+         database: config.get('database.database-name'),
+         entities: config.get('database.entities'),
+         migrations: config.get('database.migrations'),
+         synchronize: !PROD,
+         logging: !PROD
+      } as ConnectionOptions)
 
       app.use(express.json({ extended: false } as Parameters<typeof express.json>[0]))
       
@@ -19,6 +31,7 @@ const start = async () => {
       app.use('/ingredients', ingredientsRoute)
       app.use('/cocktails', cocktailsRoute)
 
+      //console.log(config.get('database.port'))
       app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
    }
    catch (err) {

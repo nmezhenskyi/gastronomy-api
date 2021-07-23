29b748a069085ca3e57b711dad36992c6020a126
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-23 12:00:59 -0400

    comment

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts string to number. If string cannot be converted, returns undefined.
+ * 
+ * @param str String to be converted
+ * @returns Number or undefined
+ */
 export const paramToInt = (str: string): number | undefined => {
    const parsed = parseInt(str)
 

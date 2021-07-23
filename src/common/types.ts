819ef030a60f412f81d26c12f5885de5c30581e2
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-23 12:01:19 -0400

    AuthRequest interface

---
+++ src/common/types.ts
@@ -0,0 +1,13 @@
+import { Request } from 'express'
+
+export interface AuthRequest extends Request {
+   user?: {
+      id: string,
+      email?: string
+   },
+   member?: {
+      id: string,
+      email: string,
+      role: string
+   }
+}

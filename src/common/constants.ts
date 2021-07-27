976bc3495442bdb904a65ed597f9bb195692a454
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-26 20:37:22 -0400

    updated constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,6 @@
+import config from 'config'
+
+export const API_URL = `http${config.get('environment.secure') ? 's' : ''}://${config.get('environment.host')}:${config.get('environment.port')}`
 export const PROD = process.env.NODE_ENV === 'production'
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || config.get('environment.port')
+

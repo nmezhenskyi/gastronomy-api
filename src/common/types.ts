90a98d987d8dbd356a601c1a7e7a1d5198a2708d
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-26 20:53:39 -0400

    comments

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,11 +1,21 @@
 import { Request } from 'express'
 
+/**
+ * Role reflects different authorization level.
+ * 
+ * - ```SUPERVISOR``` - member role with the highest access level
+ * - ```CREATOR``` - member role with the full CRUD access to *cocktails*, *meals*, and *ingredients* collections
+ * - ```USER``` - user role
+ */
 export enum Role {
    SUPERVISOR = 'Supervisor',
    CREATOR = 'Creator',
    USER = 'User'
 }
 
+/**
+ * Authenticated request for *express* middleware.
+ */
 export interface AuthRequest extends Request {
    user?: {
       id: string
@@ -16,6 +26,9 @@ export interface AuthRequest extends Request {
    }
 }
 
+/**
+ * Pair of access and refresh tokens.
+ */
 export interface TokenPair {
    accessToken: string,
    refreshToken: string

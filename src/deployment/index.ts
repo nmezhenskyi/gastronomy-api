90e74398afeb029d89a0573d38a03039438054ff
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-07-31 11:54:15 -0400

    deployment

---
+++ src/deployment/index.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata'
+import config from 'config'
+import { createConnection, ConnectionOptions } from 'typeorm'
+import { PROD } from '../common/constants'
+import { setSupervisor } from './set-supervisor'
+
+const start = async () => {
+   try {
+      const connection = await createConnection({
+         type: config.get('database.type'),
+         host: config.get('database.host'),
+         port: config.get('database.port'),
+         username: config.get('database.username'),
+         password: config.get('database.password'),
+         database: config.get('database.database-name'),
+         entities: config.get('typeorm.entities'),
+         migrations: config.get('typeorm.migrations'),
+         synchronize: !PROD,
+         logging: !PROD
+      } as ConnectionOptions)
+
+      await setSupervisor()
+
+      await connection.close()
+
+      console.log('Deployment process have been completed')
+   }
+   catch (err) {
+      console.error(err)
+   }
+}
+
+start()

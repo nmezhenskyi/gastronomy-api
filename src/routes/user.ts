8ba03f4b52b6ec201113c5de988a0bf9acb73f6c
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-08-05 16:21:58 -0400

    delete user account route: DELETE /user/profile

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -142,4 +142,21 @@ async (req: AuthRequest, res: Response) => {
    return res.status(200).json(result.body)
 })
 
+/**
+ * Delete user account.
+ * 
+ * @route   DELETE /user/profile
+ * @access  Private (User)
+ */
+router.delete('/profile', authorize(Role.USER), async (req: AuthRequest, res: Response) => {
+   if (!req.user) return res.status(404).json({ message: 'User not found' })
+
+   const result = await UserService.remove(req.user.id)
+
+   if (result.message === 'FAILED') return res.status(500).json({ message: `Couldn't delete user account` })
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: 'User not found' })
+
+   return res.status(200).json({ message: 'User has been deleted' })
+})
+
 export default router

e4567a5638bae0fcf6789d9a139c261e5b861e2f
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-08-05 22:56:16 -0400

    removed empty line

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,4 +3,3 @@ import config from 'config'
 export const API_URL = `http${config.get('environment.secure') ? 's' : ''}://${config.get('environment.host')}:${config.get('environment.port')}`
 export const PROD = process.env.NODE_ENV === 'production'
 export const PORT = process.env.PORT || config.get('environment.port')
-

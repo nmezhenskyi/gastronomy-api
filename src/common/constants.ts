9f3e17ecc914b11b7b0832bdc382176ea0c1b4de
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-08-10 15:21:15 -0400

    LOG_DIR

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,7 @@
 import config from 'config'
+import path from 'path'
 
 export const API_URL = `http${config.get('environment.secure') ? 's' : ''}://${config.get('environment.host')}:${config.get('environment.port')}`
 export const PROD = process.env.NODE_ENV === 'production'
 export const PORT = process.env.PORT || config.get('environment.port')
+export const LOG_DIR = path.join(__dirname, '../../logs/')

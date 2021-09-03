18ccc91531ff90f29f79ee9bee3a13bea7cf4d9a
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-09-02 22:06:47 -0400

    updated paramToInt

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -4,9 +4,10 @@
  * @param str String to be converted
  * @returns Number or undefined
  */
-export const paramToInt = (str: string): number | undefined => {
-   const parsed = parseInt(str)
+export const paramToInt = (str: string | undefined): number | undefined => {
+   if (!str) return undefined
 
+   const parsed = parseInt(str)
    if (isNaN(parsed)) return undefined
 
    return parsed

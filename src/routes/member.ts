794247b07a001973efef48f11857b0ac9ea687e8
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-09-24 10:32:37 -0400

    GET /member/members, /member/members/:id; GET /member/users, /member/users/:id

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -1,10 +1,23 @@
 import express, { Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { MemberService } from '../services/member-service'
+import { UserService } from '../services/user-service'
+import { paramToInt } from '../common/utils'
 import { authorize } from '../middleware/authorize'
-import { AuthRequest, Role } from '../common/types'
+import { AuthRequest, Pagination, Role } from '../common/types'
 
 const router = express.Router()
+
+interface GetMembersQuery extends Pagination {
+   firstName?: string
+   lastName?: string
+   role?: Role.CREATOR | Role.SUPERVISOR
+}
+
+interface GetUsersQuery extends Pagination {
+   name?: string
+   location?: string
+}
 
 /**
  * Register new member Creator account.
@@ -12,8 +25,7 @@ const router = express.Router()
  * @route   POST /member/register
  * @access  Private (Supervisor)
  */
-router.post('/',
-authorize(Role.SUPERVISOR),
+router.post('/', authorize(Role.SUPERVISOR),
 body('email').notEmpty().isEmail(),
 body('password').notEmpty().isLength({ min: 6, max: 50 }),
 body('firstName').notEmpty().isLength({ max: 50 }).trim(),
@@ -30,12 +42,19 @@ async (req: AuthRequest, res: Response) => {
       password: req.body.password,
       role: Role.CREATOR
    })
-
    if (result.message === 'INVALID') return res.status(400).json({ message: 'Member with this email already exists' })
    if (result.message === 'FAILED' || !result.body) return res.status(500).json({ message: 'Failed to create member account' })
 
    return res.status(200).json(result.body)
 })
+
+/**
+ * Update member information.
+ * 
+ * @route   PUT /member/profile
+ * @access  Private (Creator, Supervisor)
+ */
+//router.put('/profile', authorize([Role.CREATOR, Role.SUPERVISOR]))
 
 /**
  * Delete member account.
@@ -78,9 +97,79 @@ async (req, res) => {
       return res.status(400).json({ message: 'Invalid data in the request body', errors: errors.array() })
 
    const result = await MemberService.login(req.body.email, req.body.password)
-
    if (result.message === 'NOT_FOUND') return res.status(400).json({ message: `Wrong credentials` })
    if (result.message === 'FAILED' || !result.body) return res.status(500).json({ message: `Failed to log in` })
+
+   return res.status(200).json(result.body)
+})
+
+/**
+ * Find member accounts.
+ * 
+ * @route   GET /member/members
+ * @access  Private (Supervisor)
+ */
+router.get('/members', authorize(Role.SUPERVISOR),
+async (req: AuthRequest<GetMembersQuery>, res: Response) => {
+   const { firstName, lastName, role, offset, limit } = req.query
+   const searchBy: { firstName?: string, lastName?: string, role?: any } = {}
+   if (firstName) searchBy.firstName = firstName
+   if (lastName) searchBy.lastName = lastName
+   if (role) searchBy.role = role
+
+   const result = await MemberService.find(searchBy, paramToInt(offset), paramToInt(limit))
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: 'No members were found' })
+   if (result.message === 'FAILED') return res.status(500).json({ message: 'Failed to find members' })
+
+   return res.status(200).json(result.body)
+})
+ 
+/**
+ * Find member account by id.
+ * 
+ * @route   GET /member/members/:id
+ * @access  Private (Supervisor)
+ */
+router.get('/members/:id', authorize(Role.SUPERVISOR),
+async (req: AuthRequest, res: Response) => {
+   const result = await MemberService.findOne({ id: req.params.id })
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: 'Member not found' })
+   if (result.message === 'FAILED') return res.status(500).json({ message: 'Failed to find member' })
+
+   return res.status(200).json(result.body)
+})
+
+/**
+ * Find user accounts.
+ * 
+ * @route   GET /member/users
+ * @access  Private (Supervisor)
+ */
+router.get('/members/users', authorize(Role.SUPERVISOR),
+async (req: AuthRequest<GetUsersQuery>, res: Response) => {
+   const { name, location, offset, limit } = req.params
+   const searchBy: { name?: string, location?: string } = {}
+   if (name) searchBy.name = name
+   if (location) searchBy.location = location
+
+   const result = await UserService.find(searchBy, paramToInt(offset), paramToInt(limit))
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: 'No users were found' })
+   if (result.message === 'FAILED') return res.status(500).json({ message: 'Failed to find users' })
+
+   return res.status(200).json(result.body)
+})
+
+/**
+ * Find user account by id.
+ * 
+ * @route   GET /member/users/:id
+ * @access  Private (Supervisor)
+ */
+router.get('/members/users/:id', authorize(Role.SUPERVISOR),
+async (req: AuthRequest, res: Response) => {
+   const result = await UserService.findOne({ id: req.params.id })
+   if (result.message === 'NOT_FOUND') return res.status(404).json({ message: 'User not found' })
+   if (result.message === 'FAILED') return res.status(500).json({ message: 'Failed to find user' })
 
    return res.status(200).json(result.body)
 })

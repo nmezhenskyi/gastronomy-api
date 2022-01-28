06442a39f5abec05d1959900a300a66b6697507d
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-01-28 14:40:21 -0500

    feat(client): home

---
+++ client/src/views/home/home.tsx
@@ -0,0 +1,10 @@
+// Components:
+import { Landing } from '../../components/landing'
+
+export const Home = () => {
+   return (
+      <section id="home">
+         <Landing />
+      </section>
+   )
+}

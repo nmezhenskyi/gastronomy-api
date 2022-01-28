da1b6b370a3e5c18f6d4bae92732f0da2a7a69a7
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-01-28 14:38:23 -0500

    feat(client): setup routes

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
 // Styles:
 import './app.css'
+
+// Components:
+import { Navbar } from './components/navbar'
+import { Home } from './views/home/home'
+import { Footer } from './components/footer'
 
 export const App = () => {
    return (
       <div className="app">
-         GastronomyAPI
+         <Navbar />
+         <Routes>
+            <Route path="/" element={<Home />} />
+         </Routes>
+         <Footer />
       </div>
    )
 }

b1483a26b23c40968fcb907ecd49a360b617fe93
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-01 11:54:15 -0500

    feat(client): init recipe-gallery component

---
+++ client/src/components/recipe-gallery.tsx
@@ -0,0 +1,12 @@
+// Styles:
+import './recipe-gallery.css'
+
+export const RecipeGallery = () => {
+   return (
+      <section className="recipe-gallery">
+         <div className="container">
+            Recipes
+         </div>
+      </section>
+   )
+}

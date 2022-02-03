87cda680835074c63713913ba47b4a2801a02984
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-02 20:11:23 -0500

    feat(client - app): not-found route handler

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -6,6 +6,8 @@ import './app.css'
 // Components:
 import { Navbar } from './components/navbar'
 import { Home } from './views/home/home'
+import { Login } from './views/login/login'
+import { NotFound } from './views/not-found/not-found'
 import { Footer } from './components/footer'
 
 export const App = () => {
@@ -14,6 +16,8 @@ export const App = () => {
          <Navbar />
          <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
          </Routes>
          <Footer />
       </div>

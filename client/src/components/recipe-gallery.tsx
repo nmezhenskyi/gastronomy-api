8684d6351331802c08eaee405a38492aadbcbc70
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-02 20:10:10 -0500

    feat(client): recipe-gallery imporements
    Defined recipe-gallery & added styles

--- client/src/components/recipe-gallery.tsx
+++ client/src/components/recipe-gallery.tsx
@@ -1,11 +1,47 @@
+import { Link } from 'react-router-dom'
+
 // Styles:
 import './recipe-gallery.css'
 
-export const RecipeGallery = () => {
+interface Recipe {
+   id: string
+   name: string
+   description: string | null
+   method: string
+   notesOnIngredients?: string | null
+   notesOnExecution?: string | null
+   notesOnTaste?: string | null
+}
+
+interface Props {
+   recipes: Recipe[]
+   isPending: boolean
+   error: string | null
+}
+
+export const RecipeGallery = ({ recipes, isPending, error }: Props) => {
+   if (!recipes || recipes.length === 0) {
+      return (
+         <section className="recipe-gallery">
+            <div className="container">
+               <h3 className="error">No recipes were found</h3>
+            </div>
+         </section>
+      )
+   }
+
    return (
       <section className="recipe-gallery">
          <div className="container">
-            Recipes
+            {isPending && <p className="loading">Loading...</p> }
+            {error && <p className="error">{error}</p>}
+            {recipes && recipes.map(recipe => (
+               <div key={recipe.id} className="recipe-card">
+                  <h3>{recipe.name}</h3>
+                  <p>{recipe.description}</p>
+                  <Link to={`#`}>Read More</Link>
+               </div>
+            ))}
          </div>
       </section>
    )

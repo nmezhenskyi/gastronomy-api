c899c3df066c6b1233ab9e1f170d8c29e7afb8b8
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-12 15:16:24 -0500

    feat(client): login page

--- client/src/views/login/login.tsx
+++ client/src/views/login/login.tsx
@@ -1,32 +1,90 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { API_URL } from '../../common/constants'
+
+// Styles:
+import './login.css'
+
+type Role = 'USER' | 'MEMBER'
 
 export const Login = () => {
+   const [role, setRole] = useState<Role>('USER')
    const [email, setEmail] = useState('')
    const [password, setPassword] = useState('')
 
-   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault()
+
+      try {
+         if (role === 'USER') {
+            const res = await axios.post(`${API_URL}/user/login`,
+               { email, password })
+            console.log(res)
+         }
+         else {
+            const res = await axios.post(`${API_URL}/member/login`,
+               { email, password })
+            console.log(res)
+         }
+      }
+      catch (err: unknown) {
+         console.error(err)
+      }
    }
 
    return (
       <section id="login">
-         <form onSubmit={e => handleSubmit(e)}>
-            <label htmlFor="email">Email:</label>
-            <input
-               id="email"
-               type="text"
-               value={email}
-               onChange={e => setEmail(e.target.value)}
-            />
-            <label htmlFor="Password:">Password:</label>
-            <input
-               id="password"
-               type="password"
-               value={password}
-               onChange={e => setPassword(e.target.value)}
-            />
-            <button type="submit">Log in</button>
-         </form>
+         <div className="container">
+            <form onSubmit={e => handleSubmit(e)}>
+               <h3>Log In</h3>
+               <div className="role-toggle">
+                  <label htmlFor="user">User</label>
+                  <input
+                     id="user"
+                     type="radio"
+                     name="role"
+                     checked={role === 'USER'}
+                     value={'USER'}
+                     onChange={e => setRole(e.target.value as Role)}
+                  />
+                  <label htmlFor="member">Member</label>
+                  <input
+                     id="member"
+                     type="radio"
+                     name="role"
+                     checked={role === 'MEMBER'}
+                     value={'MEMBER'}
+                     onChange={e => setRole(e.target.value as Role)}
+                  />
+               </div>
+               <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                     id="email"
+                     type="email"
+                     value={email}
+                     onChange={e => setEmail(e.target.value)}
+                     placeholder="Enter your email"
+                  />
+               </div>
+               <div className="form-group">
+                  <label htmlFor="Password:">Password</label>
+                  <input
+                     id="password"
+                     type="password"
+                     value={password}
+                     onChange={e => setPassword(e.target.value)}
+                     placeholder="Enter your password"
+                  />
+               </div>
+               <button className="btn" type="submit">Log in</button>
+            </form>
+            <p className="hint">
+               Don't have an account yet?&nbsp;
+               <Link to="/register">Register</Link>
+            </p>
+         </div>
       </section>
    )
 }
